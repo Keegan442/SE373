fe70299c44545438c449b42a28b9ce61b84074f4
Keegan Preston <klpreston@email.neit.edu>
2024-01-30 18:42:21 -0500

    main

--- w3_assignment_1/api/index.js
+++ w3_assignment_1/api/index.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 const express = require('express');
+const { v4 } = require('uuid');
 var path = require('path');
 const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
@@ -7,6 +8,18 @@ const generateColorGrid = require('./color-generator');
 
 const app = express();
 const port = 3000;
+
+app.get('/api', (req, res) => {
+  const path = `/api/item/${v4()}`;
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
+
+app.get('/api/item/:slug', (req, res) => {
+  const { slug } = req.params;
+  res.end(`Item: ${slug}`);
+});
 
 const whitelist = [
   '*'
@@ -32,3 +45,5 @@ app.post('/generate', (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
+
+module.exports = app;
