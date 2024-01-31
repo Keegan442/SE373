098ac4fb6f96c36dad777a016f75516be68423b0
Keegan Preston <klpreston@email.neit.edu>
2024-01-31 15:19:55 -0500

    main

---
+++ w3_assignment_1/index.js
@@ -0,0 +1,36 @@
+var createError = require('http-errors');
+const express = require('express');
+var path = require('path');
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+const generateColorGrid = require('./color-generator');
+
+const app = express();
+const port = 3000;
+
+const whitelist = [
+  '*'
+];
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Set up Handlebars with '.hbs' extension
+app.engine('.hbs', exphbs({ extname: '.hbs' }));
+app.set('view engine', '.hbs');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.post('/generate', (req, res) => {
+  const gridSize = parseInt(req.body.gridSize);
+  const colors = generateColorGrid(gridSize);
+
+  res.send(colors);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
+
+module.exports = app;
