273f524c769b6c634b45275419e1d9a881352563
Keegan Preston <klpreston@email.neit.edu>
2024-01-31 15:40:22 -0500

    main

--- w3_assignment_1/index.js
+++ w3_assignment_1/index.js
@@ -6,7 +6,7 @@ const exphbs = require('express-handlebars');
 const generateColorGrid = require('./color-generator');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const whitelist = [
   '*'
